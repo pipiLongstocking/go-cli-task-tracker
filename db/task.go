78bc1e1ddae8be44660c9a6e783469af4705632a
Ravi Teja <raviteja@Ravis-MacBook-Air.local>
2025-02-24 22:07:25 +0530

    Few corrections to the code

--- db/task.go
+++ db/task.go
@@ -1,8 +1,9 @@
 package db
 
+// Task defines the structure of the task object
 type Task struct {
 	ID          uint64	`json:"id"`
 	Title       string	`json:"title"`
-	Description string	`json:"description"`
-	Done        bool	`json:"done"`
+	// IsCompleted denotes if the task has been completed
+	IsCompleted bool	`json:"done"`
 }
--- jsondb/jsondb.go
+++ jsondb/jsondb.go
@@ -58,6 +58,7 @@ func (jdb *JsonTaskDB) Connect() error {
 		if err = json.NewEncoder(jdb.file).Encode(emptyTasks); err != nil {
 			return err
 		}
+		// Set the offset to the beginning of the file
 		if _, err = jdb.file.Seek(0, 0); err != nil {
 			return err
 		}
@@ -75,10 +76,6 @@ func (jdb *JsonTaskDB) Close() error {
 		return err
 	}
 	if err := jdb.file.Close(); err != nil {
-		return err
-	}
-	err := jdb.file.Close()
-	if err != nil {
 		return err
 	}
 	jdb.file = nil
@@ -106,8 +103,7 @@ func (jdb *JsonTaskDB) getTasks() ([]*db.Task, error) {
 // writeTasks writes the tasks to the file
 func (jdb *JsonTaskDB) writeTasks(tasks []*db.Task) error {
 	// Truncate the file before writing
-	err := jdb.file.Truncate(0)
-	if err != nil {
+	if err := jdb.file.Truncate(0); err != nil {
 		return err
 	}
 	encoder := json.NewEncoder(jdb.file)
@@ -115,6 +111,7 @@ func (jdb *JsonTaskDB) writeTasks(tasks []*db.Task) error {
 	return encoder.Encode(tasks)
 }
 
+// GetTasks get all tasks from the file
 func (jdb *JsonTaskDB) GetTasks() ([]*db.Task, error) {
 	jdb.mu.RLock()
 	defer jdb.mu.RUnlock()
@@ -139,6 +136,7 @@ func (jdb *JsonTaskDB) AddTask(t *db.Task) error {
 
 }
 
+// DeleteTask deletes a task from the file
 func (jdb *JsonTaskDB) DeleteTask(taskID uint64) error {
 	jdb.mu.Lock()
 	defer jdb.mu.Unlock()
